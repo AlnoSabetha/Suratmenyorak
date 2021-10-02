293fe570f0775f00195cf511bf85396e9e28913e
AlnoSabetha <alnosabetha7@gmail.com>
2021-10-02 11:17:45 +0700

    loginPage1

---
+++ resources/views/login.blade.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+    <!-- My Style-->
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+
+    <title>Login | SuratMenyorak</title>
+  </head>
+  <body>
+    <img class="mb-4 mt-2 ms-4" src="img/logoukdw.png" alt="" width="92" height="112">
+      <div class="row d-flex justify-content-center mt-5">
+          <div class="col-md-3">
+                  <main class="form-signin">
+                    <form>
+                        <h1 class="h1 mb-3 fw-normal d-flex justify-content-center">Login</h1>
+
+                        <div class="form-floating d-flex justify-content-center">
+                            <p>Sign in and start managing your candidate</p>
+                        </div>
+
+                        <div class="form-floating">
+                        <input type="email" class="form-control btn-black" id="floatingInput" placeholder="name@example.com">
+                        <label for="floatingInput">Username</label>
+                        </div>
+
+                        <div class="form-floating mt-3">
+                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                        <label for="floatingPassword">Password</label>
+                        </div>
+
+                        <div class="checkbox mb-3">
+                        <label>
+                            <input type="checkbox" value="remember-me"> Remember me
+                        </label>
+                        </div>
+
+                        <button class="w-100 btn btn-lg btn-primary btn-success text-dark" type="submit">Login</button>
+
+                        <div class="mt-2">
+                          <a href="">Forgot password ?</a>
+                        </div>
+                    </form>
+                    </main>
+          </div>
+      </div>
+
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#00cba9" fill-opacity="1" d="M0,96L80,96C160,96,320,96,480,122.7C640,149,800,203,960,229.3C1120,256,1280,256,1360,256L1440,256L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
+    <path fill="#273036" fill-opacity="0.7" d="M0,160L60,165.3C120,171,240,181,360,181.3C480,181,600,171,720,149.3C840,128,960,96,1080,101.3C1200,107,1320,149,1380,170.7L1440,192L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+
+  </body>
+</html>
